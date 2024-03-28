d414b44552f939986bea4583467f7af4268d394e
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2024-03-28 23:48:37 +0300

    adding-a-parsing-service

---
+++ Calculator.Application/Services/Implementations/ExpressionParsingService.cs
@@ -0,0 +1,20 @@
+﻿using Calculator.Application.Services.Interfaces;
+using Calculator.Core.Extensions;
+
+namespace Calculator.Application.Services.Implementations
+{
+    public class ExpressionParsingService : IExpressionParsingService
+    {
+        private readonly ExpressionParser parser;
+
+        public ExpressionParsingService()
+        {
+            parser = new ExpressionParser();
+        }
+
+        public double EvaluateExpression(string expression)
+        {
+            return parser.ParseAndEvaluate(expression);
+        }
+    }
+}
