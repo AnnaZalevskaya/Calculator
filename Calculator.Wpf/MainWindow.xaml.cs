662b6c5c17bae54a633e9a4b346e8c6e8a3a4ec8
Vadim <vadim565162@mail.ru>
2024-04-01 10:26:38 +0300

    Add variables

--- Calculator.Wpf/MainWindow.xaml.cs
+++ Calculator.Wpf/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Calculator.Wpf
 {
@@ -19,32 +10,131 @@ namespace Calculator.Wpf
     public partial class MainWindow : Window
     {
         string textBoxVal = string.Empty;
-        char[] operationsArr = new[] { '+', '-', '*', '/' };
-        char[] numbers = new[] { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        char[] mainButtonValues = new[] { '1', '2', '3', '+', '4', '5', '6', '-', '7', '8', '9', '*', '0', '.', 'C', '/' };
+        string[] additionalButtonValues = new[] { "<-", "=", "(", ")", "->" };
+        string[] variablesName = new[] { "X", "Y", "Z", "I", "J", "K", "Save" };
+        string[] toolbarValues = new[] { "Variables", "Functions" };
+        ListBox VariablesListBox = new ListBox();
+
+        Dictionary<string, string> variablesDictionary = new Dictionary<string, string>();
+
         public MainWindow()
         {
             InitializeComponent();
-            Grid numberPanel = InitializeGridPanel(3, 3);
-            Grid operationPanel = InitializeGridPanel(4, 1);
 
-            AddButtons(numberPanel, numbers);
-            AddButtons(operationPanel, operationsArr);
 
-            Grid.SetRow(numberPanel, 1);
-            Grid.SetColumn(numberPanel, 1);
+            Grid numberPanel = InitializeGridPanel(4, 4);
+            Grid additionalPanel = InitializeGridPanel(1, 5);
+            InitializeVariablePanel();
+
+            AddButtons(numberPanel, mainButtonValues);
+            AddButtons(additionalPanel, additionalButtonValues);
+
+            SetGridPosition(numberPanel, 3, 0);
+            SetGridPosition(additionalPanel, 2, 0);
+
             Calculator.Children.Add(numberPanel);
-
-            Grid.SetRow(operationPanel, 1);
-            Grid.SetColumn(operationPanel, 0);
-            Calculator.Children.Add(operationPanel);
+            Calculator.Children.Add(additionalPanel);
+            Calculator.Children.Add(InitializeToolbar());
         }
 
         private void InputButton_Click(object sender, RoutedEventArgs e)
         {
-            textBoxVal += ((Button)sender).Content.ToString();
+            Button pressedButton = (Button)sender;
+            switch (pressedButton.Content)
+            {
+                case 'C':
+                    ClearExpression();
+                    break;
+                case "<-":
+                    if (textBoxVal != String.Empty)
+                    {
+                        textBoxVal = textBoxVal.Remove(textBoxVal.Count() - 1);
+                        ExpressionTextBox.Text = textBoxVal;
+                    }
+                    break;
+                case "Save":
+                    SaveVariable();
+                    break;
+                default:
+                    textBoxVal += pressedButton.Content;
+                    ExpressionTextBox.Text = textBoxVal;
+                    break;
+            }
+
+        }
+        private void SetGridPosition(FrameworkElement panel, int row, int colomn)
+        {
+            Grid.SetRow(panel, row);
+            Grid.SetColumn(panel, colomn);
+        }
+        void ClearExpression()
+        {
+            textBoxVal = string.Empty;
             ExpressionTextBox.Text = textBoxVal;
         }
-        private void AddButtons(Grid panel, char[] operations)
+        private void SaveVariable()
+        {
+            string[] variableKeyValue = ExpressionTextBox.Text.Split('=');
+            variablesDictionary.Add(variableKeyValue[0], variableKeyValue[1]);
+            ClearExpression();
+            AddVariableToListBox(variableKeyValue[0]);
+        }
+        private void AddVariableToListBox(string varName)
+        {
+            Grid grid = InitializeGridPanel(1, 3);
+
+            Button removeBtn = new Button();
+            Button variableBtn = new Button();
+            TextBlock variableValueTextBlock = new TextBlock();
+
+            removeBtn.Content = "Delete";
+            variableBtn.Content = varName;
+            removeBtn.Click += DeleteVar;
+            variableBtn.Click += InputButton_Click;
+
+            variableValueTextBlock.Text = variablesDictionary[varName];
+
+            Grid.SetColumn(variableValueTextBlock, 1);
+            Grid.SetColumn(removeBtn, 2);
+
+            grid.Children.Add(variableBtn);
+            grid.Children.Add(variableValueTextBlock);
+            grid.Children.Add(removeBtn);
+
+            VariablesListBox.Items.Add(grid);
+        }
+        private void DeleteVar(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            Grid grid = (Grid)b.Parent;
+            Button varB = (Button)grid.Children[0];
+            variablesDictionary.Remove(varB.Content.ToString());
+            grid.Children.Clear();
+            ListBox varLB = (ListBox)grid.Parent;
+            varLB.Items.Remove(grid);
+        }
+
+        private void InitializeVariablePanel()
+        {
+            ColumnDefinition variableColomn = new ColumnDefinition();
+            variableColomn.Width = new GridLength(2, GridUnitType.Star);
+            Calculator.ColumnDefinitions.Add(variableColomn);
+
+            Grid VariablesNamePanel = InitializeGridPanel(3, 3);
+            AddButtons(VariablesNamePanel, variablesName);
+            SetGridPosition(VariablesNamePanel, 2, 1);
+            SetGridPosition(VariablesListBox, 3, 1);
+            Calculator.Children.Add(VariablesNamePanel);
+            Calculator.Children.Add(VariablesListBox);
+
+        }
+        private void SetGridSpan(FrameworkElement panel, int rowSpan, int colomnSpan)
+        {
+            Grid.SetColumnSpan(panel, rowSpan);
+            Grid.SetColumnSpan(panel, colomnSpan);
+        }
+        private void AddButtons<T>(Grid panel, IList<T> operations)
         {
             int index = 0;
 
@@ -52,9 +142,12 @@ namespace Calculator.Wpf
             {
                 for (int colomn = 0; colomn < panel.ColumnDefinitions.Count; colomn++)
                 {
+                    if (index == operations.Count()) { return; };
+
                     Button numberBtn = new Button();
                     numberBtn.Click += InputButton_Click;
                     numberBtn.Content = operations[index];
+                    numberBtn.Margin = new Thickness(1, 3, 1, 3);
                     Grid.SetRow(numberBtn, row);
                     Grid.SetColumn(numberBtn, colomn);
                     panel.Children.Add(numberBtn);
@@ -67,7 +160,8 @@ namespace Calculator.Wpf
         private Grid InitializeGridPanel(int row, int column)
         {
             Grid gridPanel = new Grid();
-            gridPanel.ShowGridLines = true;
+            gridPanel.Margin = new Thickness(5, 5, 5, 5);
+
             for (int i = 0; i < row; i++)
             {
                 gridPanel.RowDefinitions.Add(new RowDefinition());
@@ -79,12 +173,25 @@ namespace Calculator.Wpf
             }
             return gridPanel;
         }
-
-        private void Clear_Click(object sender, RoutedEventArgs e)
+        private ToolBar InitializeToolbar()
         {
-            textBoxVal = string.Empty;
-            ExpressionTextBox.Text = textBoxVal;
-
+            ToolBar menu = new ToolBar();
+            foreach (var item in toolbarValues)
+            {
+                RadioButton checkBox = new RadioButton();
+                checkBox.Checked += RadioButton_Checked;
+                checkBox.Content = item;
+                menu.Items.Add(checkBox);
+                SetGridSpan(menu, 1, 4);
+            }
+            return menu;
         }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+        }
+
+
     }
+
 }
