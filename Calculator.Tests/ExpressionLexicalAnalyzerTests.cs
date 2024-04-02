94d0905d993c274caf715d27c35fc5ba5228e803
Yulia <93128899+Aealius@users.noreply.github.com>
2024-04-02 17:14:56 +0300

    implement unit-tests for ExpressionLexicalAnalyzer and ExpressionParser classes

---
+++ Calculator.Tests/ExpressionLexicalAnalyzerTests.cs
@@ -0,0 +1,102 @@
+﻿using Calculator.Core;
+using Calculator.Infrastructure.Data;
+using FluentAssertions;
+
+namespace Calculator.Tests
+{
+	public class ExpressionLexicalAnalyzerTests
+	{
+		private ExpressionContext context = CreateContext();
+
+		[Fact]
+		public void Tokenize_ReturnsSameListWhenArithmeticExpressionIsPassed()
+		{
+			var expression = "(2 * (2 + 4 / 5) / 3) + 5 * 4 / (2 + 7)";
+
+			var expected = new List<Token>()
+			{
+				new Token(TokenType.LeftParenthesis, "("),
+				new Token(TokenType.Number, "2"),
+				new Token(TokenType.Operator, "*"),
+				new Token(TokenType.LeftParenthesis, "("),
+				new Token(TokenType.Number, "2"),
+				new Token(TokenType.Operator, "+"),
+				new Token(TokenType.Number, "4"),
+				new Token(TokenType.Operator, "/"),
+				new Token(TokenType.Number, "5"),
+				new Token(TokenType.RightParenthesis, ")"),
+				new Token(TokenType.Operator, "/"),
+				new Token(TokenType.Number, "3"),
+				new Token(TokenType.RightParenthesis, ")"),
+				new Token(TokenType.Operator, "+"),
+				new Token(TokenType.Number, "5"),
+				new Token(TokenType.Operator, "*"),
+				new Token(TokenType.Number, "4"),
+				new Token(TokenType.Operator, "/"),
+				new Token(TokenType.LeftParenthesis, "("),
+				new Token(TokenType.Number, "2"),
+				new Token(TokenType.Operator, "+"),
+				new Token(TokenType.Number, "7"),
+				new Token(TokenType.RightParenthesis, ")")
+			};
+
+			var lexer = new ExpressionLexicalAnalyzer(context, expression);
+
+			var actual = lexer.Tokenize();
+
+			actual.Should()
+					.HaveSameCount(expected)
+					.And.BeEquivalentTo(expected);
+		}
+
+		[Fact]
+		public void Tokenize_ReturnsSameListWhenVariableIsPassed()
+		{
+			var expression = "x = 5";
+
+			var expected = new List<Token>()
+			{
+				new Token(TokenType.Variable, "x"),
+				new Token(TokenType.Number, "5"),
+			};
+
+			var lexer = new ExpressionLexicalAnalyzer(context, expression);
+
+			var actual = lexer.Tokenize();
+
+			actual.Should()
+					.HaveSameCount(expected)
+					.And.BeEquivalentTo(expected);
+		}
+
+		[Fact]
+		public void Tokenize_ReturnsSameListWhenFunctionIsPassed()
+		{
+			var expression = "f(x) = x + 1";
+
+			var expected = new List<Token>()
+			{
+				new Token(TokenType.Function, "f"),
+				new Token(TokenType.Variable, "x"),
+				new Token(TokenType.Operator, "+"),
+				new Token(TokenType.Variable, "1"),
+			};
+
+			var context = new ExpressionContext();
+			var lexer = new ExpressionLexicalAnalyzer(context, expression);
+
+			var actual = lexer.Tokenize();
+
+			actual.Should()
+					.HaveSameCount(expected)
+					.And.BeEquivalentTo(expected);
+
+		}
+
+		private static ExpressionContext CreateContext() 
+		{ 
+			return new ExpressionContext(); 
+		}	
+
+	}
+}
---
+++ Calculator.Tests/ExpressionParserTests.cs
@@ -0,0 +1,81 @@
+﻿using Calculator.Core.Extensions;
+using Calculator.Infrastructure.Data;
+
+namespace Calculator.Tests
+{
+	public class ExpressionParserTests
+	{
+		private readonly ExpressionParser parser = CreateExpressionParser();
+
+		[Fact]
+		public void ParseAndEvaluate_ReturnsSameValuesIfArithmeticExpressionWithPositiveValuesIsPassed()
+		{
+			var expression = "2 + 1 - 3";
+			var expected = 0d;
+
+			var actual = parser.ParseAndEvaluate(expression);
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void ParseAndEvaluate_ReturnsSameValuesIfArithmeticExpressionWithNegativeValuesIsPassed()
+		{
+			var expression = "-2 + 1 - 3";
+			var expected = -4d;
+
+			var actual = parser.ParseAndEvaluate(expression);
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void ParseAndEvaluate_ReturnsNaNIfArithmeticExpessionIsNotValid()
+		{
+			string expression = "1 +- 2 -- 3";
+			var actual = parser.ParseAndEvaluate(expression);
+			Assert.Equal(double.NaN, actual);
+		}
+
+		[Fact]
+		public void ParseAndEvaluate_ReturnsRightValueIfFunctionIsPassed()
+		{
+			string expression = "1 + f";
+			var actual = parser.ParseAndEvaluate(expression);
+			Assert.Equal(4d, actual);
+		}
+
+		[Fact]
+		public void ParseAndEvaluate_ReturnsRightValueIfVariableIsPassed()
+		{
+			string expression = "1 + x";
+			var actual = parser.ParseAndEvaluate(expression);
+			Assert.Equal(2d, actual);
+		}
+
+		[Fact]
+		public void ParseAndEvaluate_ReturnsRightValueIfVariableAndFunctionIsPassed()
+		{
+			string expression = "f + x + y";
+			var actual = parser.ParseAndEvaluate(expression);
+			Assert.Equal(6d, actual);
+		}
+
+		[Fact]
+		public void ParseAndEvaluate_ReturnsNaNIfVariableOrFunctionIsNotExist()
+		{
+			string expression = "1 + z";
+			var actual = parser.ParseAndEvaluate(expression);
+			Assert.Equal(double.NaN, actual);
+		}
+
+		private static ExpressionParser CreateExpressionParser()
+		{
+			var context = new ExpressionContext();
+
+			context.AddVariable("x", 1d);
+			context.AddVariable("y", 2d);
+			context.AddFunction("f", (x, y) => x + y);
+
+			return new ExpressionParser(context);
+		}
+	}
+}
